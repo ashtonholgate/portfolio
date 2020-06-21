402ca81f53e47d9c4b29869276df56e6ffa1584a
Ashton <ashton.holgate@luxtripper.co.uk>
2020-06-21 13:42:01 +0100

    Updates

---
+++ precache-manifest.97b412c612a5b5e7f9410ba105415199.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664339105c999db3d90774f53764f180",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "cb130686fe10f16f2a54",
+    "url": "/portfolio2020/static/css/main.71f8b655.chunk.css"
+  },
+  {
+    "revision": "be3df187ce399543976d",
+    "url": "/portfolio2020/static/js/2.c66d7165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.c66d7165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb130686fe10f16f2a54",
+    "url": "/portfolio2020/static/js/main.5045d8d3.chunk.js"
+  },
+  {
+    "revision": "2f2f9552a859c52bd04e",
+    "url": "/portfolio2020/static/js/runtime-main.626836e7.js"
+  },
+  {
+    "revision": "9700a6f6bbba7bc14e382e458270c448",
+    "url": "/portfolio2020/static/media/WWYWE.9700a6f6.png"
+  },
+  {
+    "revision": "ce9ccdcf13b3edaf57d44a518d1b207d",
+    "url": "/portfolio2020/static/media/WWYWL.ce9ccdcf.png"
+  },
+  {
+    "revision": "9039dd91a8500a08b2322f849460cc26",
+    "url": "/portfolio2020/static/media/quote.9039dd91.png"
+  }
+]);
